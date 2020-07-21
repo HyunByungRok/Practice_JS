df38b437272274d9f8925c14d1f321224815eaa7
hyunbyungrok <hyunbyungrok@gmail.com>
2020-07-22 03:04:55 +0900

    [풀이] JS_크레인인형뽑기게임
    한번 풀어봤던 문제를 JS 알고리즘 공부겸 풀어보았습니다.
    JS에서 stack이나 queue등을 어떻게 사용해야할지 알게되었습니다.
    문제는 그냥 주어진 조건을 완수하면서 스택에 데이터를 넣어 일치하면 지워나가는 방식으로 풀었습니다.

--- Algorithm/크레인 인형뽑기 게임.js
+++ Algorithm/크레인 인형뽑기 게임.js
@@ -1,4 +1,39 @@
+var board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]];
+var moves = [1,5,3,5,1,2,1,4];
+
+
 function solution(board, moves) {
-    var answer = 0;
+    var answer = 0
+
+    var moved = []
+
+    for(let move of moves) {
+        var catched = findBlock(move-1, board)
+        if(catched===0) continue;
+        if(moved[moved.length-1]===undefined) moved.push(catched)
+        else {
+            if(moved[moved.length-1]===catched) {
+                moved.pop()
+                answer+=2
+                continue
+            }
+            moved.push(catched)
+        }
+    }
+
     return answer;
 }
+
+function findBlock(move, board) {
+    let rtv=0;
+    for(let i=0; i<board.length; i++) {
+        var num = board[i][move]
+        if(num===0) continue;
+        board[i][move] = 0
+        rtv = num
+        break;
+    }
+    return rtv
+}
+
+console.log(solution(board, moves))
