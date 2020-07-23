85d002dfb85f0fef2192fe2135e13dd3ceb186a3
hyunbyungrok <hyunbyungrok@gmail.com>
2020-07-23 19:24:43 +0900

    [풀이]문자열 다루기 기본
    문자열을 다루는 기본 문제였습니다. length와 split을 사용하여 해결 하였습니다.

---
+++ Algorithm/문자열 다루기 기본.js
@@ -0,0 +1,15 @@
+var s = "10 234";
+
+function solution(s) {
+    var answer = true;
+    if(s.length==4 || s.length==6){
+        for(let c of s.split("")){
+            if("0"<=c && c<="9") continue;
+            answer = false;
+            break;
+        }
+    }else answer = false;
+    return answer;
+}
+
+console.log(solution(s));
