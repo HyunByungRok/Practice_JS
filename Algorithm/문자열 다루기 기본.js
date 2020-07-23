7a4efafce801ad29283a374ce17a14e18ed71268
hyunbyungrok <hyunbyungrok@gmail.com>
2020-07-23 19:26:08 +0900

    [수정] 문자열 다루기 기본
    굳이 split안하고 그냥 for of문을 돌려도 문자열에서 문자를 얻을 수 있어서 수정하였습니다.

--- Algorithm/문자열 다루기 기본.js
+++ Algorithm/문자열 다루기 기본.js
@@ -3,7 +3,7 @@ var s = "10 234";
 function solution(s) {
     var answer = true;
     if(s.length==4 || s.length==6){
-        for(let c of s.split("")){
+        for(let c of s){
             if("0"<=c && c<="9") continue;
             answer = false;
             break;
