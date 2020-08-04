088292393bb7c03bdf74db1410158cb8dbaaab3a
hyunbyungrok <hyunbyungrok@gmail.com>
2020-08-05 04:22:08 +0900

    [수정] 구명보트
    굳이 함수를 사용하지않고 solution에서 해결도록 바꿔서 가독성을 높였습니다.

--- Algorithm/level2/구명보트.js
+++ Algorithm/level2/구명보트.js
@@ -1,17 +1,12 @@
 var people = [70, 50, 80, 50];
 var limit = 100;
 function solution(people, limit) {
-    return countCombined(people.sort((a,b) => b-a), limit);
-}
-
-function countCombined(overLimit, limit) {
-    let start = 0, end = overLimit.length-1, count=0;
-    for(;start<=end;){
-        if(overLimit[start] + overLimit[end] <= limit) {
+    let count=0;
+    people.sort((a,b) => b-a);
+    for(let start=0, end = people.length-1;start<=end;count++, start++){
+        if(people[start] + people[end] <= limit) {
             end--;
         }
-        count++;
-        start++;
     }
     return count;
 }
