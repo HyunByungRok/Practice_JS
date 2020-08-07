ae68a8dbac08f993148baa2926e307e3f36113db
hyunbyungrok <hyunbyungrok@gmail.com>
2020-08-08 02:33:53 +0900

    [풀이] 피보나치 수
    처음 0과 1 번째 값을 넣어 둔 후  for문을 돌려서 값을 구하였습니다.

---
+++ Algorithm/level2/피보나치 수.js
@@ -0,0 +1,13 @@
+var n = 5;
+
+function solution(n) {
+    let F = [];
+    F[0] = 0;
+    F[1] = 1;
+    for(let i=2; i<=n; i++){
+        F[i] = F[i-1] + F[i-2];
+    }
+    return F[n];
+}
+
+console.log(solution(n));
