9092fd57b478d8e03a1162b95f600b5494a08831
hyunbyungrok <hyunbyungrok@gmail.com>
2020-08-09 03:18:52 +0900

    [수정] 1234567로 나누느 것이 누락된 체 커밋되어서 이 점을 수정

--- Algorithm/level2/피보나치 수.js
+++ Algorithm/level2/피보나치 수.js
@@ -5,7 +5,7 @@ function solution(n) {
     F[0] = 0;
     F[1] = 1;
     for(let i=2; i<=n; i++){
-        F[i] = F[i-1] + F[i-2];
+        F[i] = (F[i-1] + F[i-2])%1234567;
     }
     return F[n];
 }
