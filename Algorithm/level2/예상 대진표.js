a029c088b68428cea773e5765c0b39e5a7ce4507
hyunbyungrok <hyunbyungrok@gmail.com>
2020-08-10 15:46:42 +0900

    [풀이] 예상 대진표
    각 대진을 나타내는 짝 수를 2로 나누어가면 다음 대진을 붙을 상대를 알 수 있는데 이렇게 2씩 나누어 수가 같아질때까지 반복합니다.

---
+++ Algorithm/level2/예상 대진표.js
@@ -0,0 +1,16 @@
+var N = 8;
+var A = 4;
+var B = 5; 
+
+function solution(N, A, B) {
+    [A, B] = [A, B].sort((a,b) => a-b);
+    let count = 0;
+    while(B!==A) {
+        B = B%2 === 0 ? B/2 : (B+1)/2;
+        A = A%2 === 0 ? A/2 : (A+1)/2;
+        count++;
+    }
+    return count;
+}
+
+console.log(solution(N, A, B));
