a91fab4e5e0f3b6cfa717a8a3f4bd6fda41034e8
hyunbyungrok <hyunbyungrok@gmail.com>
2020-08-18 03:01:11 +0900

    [풀이] 네트워크
    모든  점에서 탐색을 돌리며 방문체크를 해주고 방문이 안된 점은 카운트해주며 다시 탐색을 반복하였습니다. 탐색에는 깊이 우선 탐색(dfs)를 사용하여 풀었습니다.

---
+++ Algorithm/level3/네트워크.js
@@ -0,0 +1,22 @@
+var n = 3;
+var computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]];
+
+function solution(n, computers) {
+    let visited = Array.from(Array(n), () => false);
+    return computers.reduce((a,c,i) => {
+        if(visited[i]) return a;
+        visited[i] = true;
+        dfs(i, visited, computers);
+        return a+1;
+    }, 0);
+}
+
+function dfs(cur, visited, computers) {
+    for(let i=0; i<computers[cur].length; i++) {
+        if(computers[cur][i] == 0 || visited[i]) continue;
+        visited[i] = true;
+        dfs(i, visited, computers);
+    }
+}
+
+console.log(solution(n, computers));
