85a0f50059fbef6ca53c7fba6b0318d9c272a1ef
hyunbyungrok <hyunbyungrok@gmail.com>
2020-08-20 12:49:04 +0900

    [풀이] 베스트앨범
    map으로 각 장르가 얼마나 재생된지 파악합니다. 그 후에 그 값과 plays의 값 index를 기준으로 정렬을 한 후  정렬된 배열을 돌면서 각 장르당 2곡의 노래를 뽑아내서 해결하였습니다.

---
+++ Algorithm/level3/베스트앨범.js
@@ -0,0 +1,48 @@
+var genres = ["classic", "pop", "classic", "classic", "pop"];
+var plays = [500, 600, 150, 800, 2500];
+
+function solution(genres, plays) {
+    let map = new Map();
+    
+    let arr = convertArr(genres, plays, map);
+
+    arr.sort(comp(map));
+
+    return getAnswer(arr);
+}
+
+function comp(map) {
+    return (a, b) => {
+        if(a[0] === b[0]) {
+            if(a[1] === b[1]) return a[2] - b[2];
+            return b[1] - a[1];
+        }
+        return map.get(b[0]) - map.get(a[0]);
+    };
+}
+
+function convertArr(genres, plays, map) {
+    return genres.map((e,i) => {
+        if(map.has(e)) map.set(e, map.get(e) + plays[i]);
+        else map.set(e, plays[i]);
+        return [e, plays[i], i];
+    });
+}
+
+function getAnswer(arr) {
+    let count = 0, key = "";
+    return arr.reduce((a, c) => {
+        let [genre, plays, idx] = c;
+        if(key === genre && count <1){
+            a.push(idx);
+            count+=1;
+        }else if(key !== genre){
+            key = genre;
+            count = 0;
+            a.push(idx);
+        }
+        return a;
+    }, []);
+}
+
+console.log(solution(genres, plays));
